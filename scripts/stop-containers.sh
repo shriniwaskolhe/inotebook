a15e09fb11c41a9b1b3832f0c20d42971b634c0c
Shriniwas Kolhe <shriniwaskolhe7218@gmail.com>
2024-08-06 17:54:11 +0530

    hh

--- scripts/stop-containers.sh
+++ scripts/stop-containers.sh
@@ -5,18 +5,18 @@
 set -e
 
 # Navigate to the correct directory
-cd /home/mubin/devopspipeline
+##cd /home/mubin/devopspipeline
 
 # Copy build files to the nginx directory
-sudo cp -r build/* nginx
+##sudo cp -r build/* nginx
 
 # Check if Docker is running and start it if necessary
-if ! docker info > /dev/null 2>&1; then
-    echo "Docker is not running. Starting Docker..."
-    sudo systemctl start docker || { echo "Failed to start Docker"; exit 1; }
-fi
+##if ! docker info > /dev/null 2>&1; then
+  ##  echo "Docker is not running. Starting Docker..."
+   ## sudo systemctl start docker || { echo "Failed to start Docker"; exit 1; }
+##fi
 
 # Stop and remove Docker containers
-sudo docker-compose down || { echo "Failed to stop containers"; exit 1; }
+##sudo docker-compose down || { echo "Failed to stop containers"; exit 1; }
 
-echo "Containers stopped successfully"
+##echo "Containers stopped successfully"
