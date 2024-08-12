3c904cec4a2fd861562bcf2c05a03428fc9a704c
Shriniwas Kolhe <shriniwaskolhe7218@gmail.com>
2024-08-12 19:23:37 +0530

    one more commit

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/inotebook"
+const mongoURI = "mongodb://shriniwas:<shriniwas>@docdb-2024-08-12-13-49-00.cluster-cvgay0a0u2gd.ap-south-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
 
 
 
