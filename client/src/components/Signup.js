31554d3af388a7e9d18ac97738f8f08cbe538966
Shriniwas Kolhe <shriniwaskolhe7218@gmail.com>
2024-08-16 19:16:11 +0530

    all c

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -7,7 +7,7 @@ const Signup = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
          const {name, email, password,}= credentials;
-         const response = await fetch("http://localhost:5000/api/auth/createuser", {
+         const response = await fetch("http://13.60.21.89:5000/api/auth/createuser", {
             
             method: 'POST',
             headers: {
